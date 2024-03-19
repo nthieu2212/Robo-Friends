99def42fbfe7101936db75f0aa8c9ae9bff7a39f
nthieu2212 <nthieu2212@gmail.com>
2024-03-19 23:11:36 +0700

    robot friends

---
+++ src/robots.js
@@ -0,0 +1,33 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'kakarot',
+        username: 'SuperSayan',
+        email: 'goku@gmail.com'    
+    },
+    {
+        id: 2,
+        name: 'vegetable',
+        username: 'prince of sayan',
+        email: 'vegita@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'bulma',
+        username: 'best wife',
+        email: 'bulma@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'krilin',
+        username: 'superhuman',
+        email: 'krilin@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Tenshinhan',
+        username: 'GodEye',
+        email: 'tenshin@gmail.com'
+    }
+]
+    
